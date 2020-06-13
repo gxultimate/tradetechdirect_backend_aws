caa0c674e753bccdc24176a9b1638b7df9f7a177
ralph <alquisadaralph@gmail.com>
2020-06-13 21:12:43 +0800

    server.js

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ app.use(function(err, req, res, next) {
 });
 
 connectDB();
-app.listen(Port , () => console.log('Server Starting.....'))
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
 
-module.exports = app;
